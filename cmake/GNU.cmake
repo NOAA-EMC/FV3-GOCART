1cbee0e6fba484301e78a9abb952f4bea72532f5
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2020-09-01 15:56:35 -0500

    Merge ufs-weather-model branch 'develop' with develop.

---
+++ cmake/GNU.cmake
@@ -0,0 +1,56 @@
+
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fcray-pointer -ffree-line-length-none -fno-range-check -fbacktrace")
+
+
+if(DEBUG)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wall -O0 -ggdb -fno-unsafe-math-optimizations -frounding-math -fsignaling-nans -ffpe-trap=invalid,zero,overflow -fbounds-check")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -O0 -ggdb")
+    add_definitions(-DDEBUG)
+elseif(REPRO)
+    if (APPLE)
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -O0 -ggdb -fno-range-check")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -ggdb")
+    else()
+        set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -O2 -ggdb -fno-range-check")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -ggdb")
+    endif()
+    add_definitions(-DREPRO)
+else()
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -O2 -fno-range-check")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
+endif()
+
+
+if(32BIT)
+    add_definitions(-DOVERLOAD_R4)
+    add_definitions(-DOVERLOAD_R8)
+else()
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fdefault-real-8 -fdefault-double-8")
+endif()
+
+
+if(OPENMP)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fopenmp")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fopenmp")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fopenmp")
+    add_definitions(-DOPENMP)
+endif()
+
+if(AVX2)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+elseif(SIMDMULTIARCH)
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+else()
+    set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+endif()
+
+if(QUAD_PRECISION)
+    add_definitions(-DENABLE_QUAD_PRECISION)
+endif()
+
+if(MULTI_GASES)
+    add_definitions(-DMULTI_GASES)
+endif()
---
+++ tests/run_test.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+set -eux
+
+echo "PID=$$"
+SECONDS=0
+
+trap '[ "$?" -eq 0 ] || write_fail_test' EXIT
+trap 'echo "run_test.sh interrupted PID=$$"; cleanup' INT
+trap 'echo "run_test.sh terminated PID=$$";  cleanup' TERM
+
+cleanup() {
+  [[ $ROCOTO = 'false' ]] && interrupt_job
+  trap 0
+  exit
+}
+
+write_fail_test() {
+  if [[ ${UNIT_TEST} == true ]]; then
+    echo ${TEST_NR} $TEST_NAME >> $PATHRT/fail_unit_test
+  else
+    echo "${TEST_NAME} ${TEST_NR} failed in run_test" >> $PATHRT/fail_test
+  fi
+  exit 1
+}
+
+if [[ $# != 5 ]]; then
+  echo "Usage: $0 PATHRT RUNDIR_ROOT TEST_NAME TEST_NR COMPILE_NR"
+  exit 1
+fi
+
+export PATHRT=$1
+export RUNDIR_ROOT=$2
+export TEST_NAME=$3
+export TEST_NR=$4
+export COMPILE_NR=$5
+
+cd ${PATHRT}
+
+[[ -e ${RUNDIR_ROOT}/run_test_${TEST_NR}.env ]] && source ${RUNDIR_ROOT}/run_test_${TEST_NR}.env
+source default_vars.sh
+source tests/$TEST_NAME
+[[ -e ${RUNDIR_ROOT}/unit_test_${TEST_NR}.env ]] && source ${RUNDIR_ROOT}/unit_test_${TEST_NR}.env
+
+# Save original CNTL_DIR name as INPUT_DIR for regression
+# tests that try to copy input data from CNTL_DIR
+export INPUT_DIR=${CNTL_DIR}
+# Append RT_SUFFIX to RUNDIR, and BL_SUFFIX to CNTL_DIR
+export RUNDIR=${RUNDIR_ROOT}/${TEST_NAME}${RT_SUFFIX}
+export CNTL_DIR=${CNTL_DIR}${BL_SUFFIX}
+
+export JBNME=$(basename $RUNDIR_ROOT)_${TEST_NR}
+
+UNIT_TEST=${UNIT_TEST:-false}
+if [[ ${UNIT_TEST} == false ]]; then
+  REGRESSIONTEST_LOG=${LOG_DIR}/rt_${TEST_NR}_${TEST_NAME}${RT_SUFFIX}.log
+else
+  REGRESSIONTEST_LOG=${LOG_DIR}/ut_${TEST_NR}_${TEST_NAME}${RT_SUFFIX}.log
+fi
+export REGRESSIONTEST_LOG
+
+echo "Test ${TEST_NR} ${TEST_NAME} ${TEST_DESCR}"
+
+source rt_utils.sh
+source atparse.bash
+
+mkdir -p ${RUNDIR}
+cd $RUNDIR
+
+###############################################################################
+# Make configure and run files
+###############################################################################
+
+# FV3 executable:
+cp ${PATHRT}/fv3_${COMPILE_NR}.exe                 fv3.exe
+
+# modulefile for FV3 prerequisites:
+cp ${PATHRT}/modules.fv3_${COMPILE_NR}             modules.fv3
+
+# Get the shell file that loads the "module" command and purges modules:
+cp ${PATHRT}/../NEMS/src/conf/module-setup.sh.inc  module-setup.sh
+cp ${PATHTR}/parm/post_itag itag
+cp ${PATHTR}/parm/postxconfig-NT.txt postxconfig-NT.txt
+cp ${PATHTR}/parm/postxconfig-NT_FH00.txt postxconfig-NT_FH00.txt
+cp ${PATHTR}/parm/params_grib2_tbl_new params_grib2_tbl_new
+
+SRCD="${PATHTR}"
+RUND="${RUNDIR}"
+
+atparse < ${PATHRT}/fv3_conf/${FV3_RUN:-fv3_run.IN} > fv3_run
+
+atparse < ${PATHTR}/parm/${INPUT_NML:-input.nml.IN} > input.nml
+
+atparse < ${PATHTR}/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
+
+atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+
+if [[ "Q${INPUT_NEST02_NML:-}" != Q ]] ; then
+    atparse < ${PATHTR}/parm/${INPUT_NEST02_NML} > input_nest02.nml
+fi
+
+# Set up the run directory
+source ./fv3_run
+
+if [[ $SCHEDULER = 'pbs' ]]; then
+  NODES=$(( TASKS / TPN ))
+  if (( NODES * TPN < TASKS )); then
+    NODES=$(( NODES + 1 ))
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_qsub.IN > job_card
+elif [[ $SCHEDULER = 'slurm' ]]; then
+  NODES=$(( TASKS / TPN ))
+  if (( NODES * TPN < TASKS )); then
+    NODES=$(( NODES + 1 ))
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_slurm.IN > job_card
+elif [[ $SCHEDULER = 'lsf' ]]; then
+  if (( TASKS < TPN )); then
+    TPN=${TASKS}
+  fi
+  NODES=$(( TASKS / TPN ))
+  if (( NODES * TPN < TASKS )); then
+    NODES=$(( NODES + 1 ))
+  fi
+  atparse < $PATHRT/fv3_conf/fv3_bsub.IN > job_card
+fi
+
+atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+
+################################################################################
+# Submit test job
+################################################################################
+
+if [[ $ROCOTO = 'false' ]]; then
+  submit_and_wait job_card
+else
+  chmod u+x job_card
+  ./job_card
+fi
+
+check_results
+
+################################################################################
+# End test
+################################################################################
+
+elapsed=$SECONDS
+echo "Elapsed time $elapsed seconds. Test ${TEST_NAME}"
